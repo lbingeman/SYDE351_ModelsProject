d4895e29e486f0c0efe1a0ac0f31dfe64a2c7b44
Laura Bingeman <lbingeman@Lauras-MacBook.lan>
2017-07-24 16:43:27 -0400

    Updated bond graph

--- DoublePendulum.m
+++ DoublePendulum.m
@@ -1,20 +1,16 @@
 function dydt = DoublePendulum(t,y,l1,l2,m1,m2,k21,b1,b2)
-    dydt = zeros(7,1);
+    dydt = zeros(5,1);
     g = 9.81;
     % y(1) - p17 theta direction
-    % y(2) - p14 - r direction
-    % y(3) - p37 - theata direction
-    % y(4) - p34 - r direction
-    % y(5) - x21
-    % y(6) - theta1
-    % y(7) - theta2
+    % y(2) - p37 - theata direction
+    % y(3) - x21
+    % y(4) - theta1
+    % y(5) - theta2
     
-    dydt(1) = -dydt(6)*y(2)-sin(y(6))*m1*g-k21*y(5)-(b1/m1)*y(1);
-    dydt(2) = dydt(6)*y(1)+cos(y(6))*m1*g;
-    dydt(3) = -dydt(7)*y(4)-sin(y(7))*m1*g+k21*y(5)-(b2/m2)*y(3);
-    dydt(4) = dydt(7)*y(3)+cos(y(7))*m1*g;
-    dydt(5) = y(1)/m1 - y(3)/m2;
-    dydt(6) = y(1)/(m1*l1); %w1
-    dydt(7) = y(3)/(m2*l2); %w2
+    dydt(1) = -sin(y(4))*m1*g-k21*y(3)-(b1/m1)*y(1);
+    dydt(2) = -sin(y(5))*m2*g+k21*y(3)-(b2/m2)*y(2);
+    dydt(3) = y(1)/m1 - y(2)/m2;
+    dydt(4) = y(1)/(m1*l1); %w1
+    dydt(5) = y(2)/(m2*l2); %w2
     
 end
